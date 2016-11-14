71777ba2e570c6cb8199c6320bcabbe5f378571f
Amir Goldstein <amir73il@gmail.com>
2016-11-14 15:39:09 +0200

    Add tests for moving a populated directory into another
    Including a test that renames inside same directory
    and then moves to another and another test that first
    moves dir into another directory and then renames in same dir.
    
    These tests are for exercising relative and absolute redirect.
    
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>

--- tests/rename-move-dir.py
+++ tests/rename-move-dir.py
@@ -6,15 +6,65 @@ from errno import *
 #
 ###############################################################################
 
-# Move a directory into another
+# Move empty directory into another
 def subtest_1(ctx):
-    """Move dir into another"""
+    """Move empty dir into another"""
     d = ctx.empty_dir()
     n = d[d.rfind("/"):]
     d = d + ctx.termslash()
-    d2 = ctx.non_empty_dir() + ctx.termslash()
+    d2 = ctx.non_empty_dir() + ctx.termslash() + n
 
-    ctx.rename(d, d2 + n)
-    ctx.rename(d, d2 + n, err=ENOENT)
-    ctx.open_dir(d2 + n, ro=1)
+    ctx.rename(d, d2)
+    ctx.rename(d, d2, err=ENOENT)
+    ctx.open_dir(d2, ro=1)
     ctx.open_dir(d, ro=1, err=ENOENT)
+
+# Move a populated directory into another
+def subtest_2(ctx):
+    """Move populated dir into another"""
+    d = ctx.non_empty_dir()
+    n = d[d.rfind("/"):]
+    d = d + ctx.termslash()
+    d2 = ctx.empty_dir() + ctx.termslash() + n
+
+    ctx.rename(d, d2)
+    ctx.rename(d, d2, err=ENOENT)
+    ctx.open_dir(d2, ro=1)
+    ctx.open_dir(d, ro=1, err=ENOENT)
+    ctx.open_file(d2 + "/a", ro=1)
+
+# Move a populated directory into another after rename in origin dir
+def subtest_3(ctx):
+    """Rename populated dir and move into another"""
+    d = ctx.non_empty_dir()
+    n = d[d.rfind("/"):]
+    d2 = d + "x" + ctx.termslash()
+    d = d + ctx.termslash()
+    d3 = ctx.empty_dir() + ctx.termslash() + n
+
+    ctx.rename(d, d2)
+    ctx.rename(d, d2, err=ENOENT)
+    ctx.rename(d2, d3)
+    ctx.rename(d2, d3, err=ENOENT)
+    ctx.open_dir(d, ro=1, err=ENOENT)
+    ctx.open_dir(d2, ro=1, err=ENOENT)
+    ctx.open_dir(d3, ro=1)
+    ctx.open_file(d3 + "/a", ro=1)
+
+# Move a populated directory into another and rename in other dir
+def subtest_4(ctx):
+    """Move populated dir into another and rename"""
+    d = ctx.non_empty_dir()
+    n = d[d.rfind("/"):]
+    d = d + ctx.termslash()
+    d2 = ctx.empty_dir() + ctx.termslash() + n
+    d3 = ctx.empty_dir() + ctx.termslash() + n + "x"
+
+    ctx.rename(d, d2)
+    ctx.rename(d, d2, err=ENOENT)
+    ctx.rename(d2, d3)
+    ctx.rename(d2, d3, err=ENOENT)
+    ctx.open_dir(d, ro=1, err=ENOENT)
+    ctx.open_dir(d2, ro=1, err=ENOENT)
+    ctx.open_dir(d3, ro=1)
+    ctx.open_file(d3 + "/a", ro=1)
