924a8a5d29bb766a1044cfcbd14cbd6ba635a1bf
Amir Goldstein <amir73il@gmail.com>
2017-04-24 11:10:19 +0300

    fix check_layer() for contant st_dev/st_ino across copy-up
    overlay inode may have lower st_dev even if it is upper.
    
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>

--- context.py
+++ context.py
@@ -519,8 +519,6 @@ class test_context:
         elif dev == self.lower_fs():
             if dentry.is_dir():
                 raise TestError(name + ": Directory unexpectedly on lower filesystem")
-            if dentry.on_upper() and self.lower_fs() != self.upper_fs():
-                raise TestError(name + ": Test file not on upper filesystem")
         elif dentry.is_dir():
             if dev != self.upper_dir_fs():
                 raise TestError(name + ": Directory not on union layer")
