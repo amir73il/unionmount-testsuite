afe2aa67a2350f69448d66d79ea2a6ea95fdb8c8
Amir Goldstein <amir73il@gmail.com>
2020-06-18 11:47:59 +0300

    Verify integrity of all snapshots with ./run --sn --verify
    Make full backup copy of lower layer on every rotate upper.
    The location of the full backups is /base/b/{0..N}, where
    /base/b/0 is the backup that was created during test setup.
    
    At the end of the test run, compare all the snapshots to all
    the full backups.
    
    Allow files only in backup, but not only in snapshot, because
    rename of directory can look like recursive remove in snapshot.
    
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>

--- remount_union.py
+++ remount_union.py
@@ -60,6 +60,12 @@ def remount_union(ctx, rotate_upper=False, cycle_mount=False):
             curr_snapshot = snapshot_mntroot + "/" + ctx.curr_layer()
             if rotate_upper:
                 os.mkdir(curr_snapshot)
+            # --sn --remount implies start with nosnapshot setup until first recycle
+            # make first backup on first cycle instead of after setup and after
+            # every rotate of upper
+            if cfg.is_verify() and (rotate_upper or not ctx.have_backup()):
+                ctx.make_backup()
+
             if rotate_upper or cycle_mount:
                 mntopt += ",redirect_dir=origin"
                 # This is the latest snapshot of lower_mntroot:
