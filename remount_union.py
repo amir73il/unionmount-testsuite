a47c72a93e9396b545040605f63b3fc0cebb0be4
Amir Goldstein <amir73il@gmail.com>
2021-03-01 13:05:37 +0200

    Test concurrent overlay snapshot watches
    Keep old snapshot watches mounted unless mount cycle was requested.
    Changes are recorded to all old snapshots, so they are differential
    to first backup.
    
    Old snapshot fs implementation does not support concurrent snapshots
    so in that case and in case of --recycle, we need to stack the old
    incremental snapshots as lower layers to get the diff from first backup.
    
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>

--- remount_union.py
+++ remount_union.py
@@ -1,6 +1,6 @@
 from tool_box import *
 
-def remount_union(ctx, rotate_upper=False, cycle_mount=False):
+def remount_union(ctx, rotate_upper=False, cold_cache=False):
     cfg = ctx.config()
     union_mntroot = cfg.union_mntroot()
     lower_mntroot = cfg.lower_mntroot()
@@ -19,12 +19,18 @@ def remount_union(ctx, rotate_upper=False, cycle_mount=False):
         first_snapshot = True
 
     # --sn default behavior is remount unless --recycle was requested
+    cycle_mount = False
     if not cfg.testing_snapshot() or ctx.remount() is False:
+        cycle_mount = True
+    # With snapshot fs mount we must cycle mount to clean cache
+    # With overlay snapshot watch it is enough to drop caches
+    if cfg.is_metacopy() and cold_cache:
         cycle_mount = True
 
     if cfg.testing_overlayfs() or cfg.testing_snapshot():
         if cycle_mount:
             system("umount " + cfg.union_mntroot())
+        if cycle_mount or cold_cache:
             system("echo 3 > /proc/sys/vm/drop_caches")
             check_not_tainted()
 
@@ -50,7 +56,12 @@ def remount_union(ctx, rotate_upper=False, cycle_mount=False):
             workdir = layer_mntroot + "/w"
 
         if cfg.testing_snapshot():
-            if rotate_upper or cycle_mount:
+            # Keep old snapshots mounted unless mount cycle was requested
+            # Old snapshot fs implementation does not support concurrent snapshots
+            # so in that case and in case of cycle_mount, we need to re-stack old
+            # snapshots as lower layers to get the desired outcome
+            restack_old_snapshots = (rotate_upper and cfg.is_metacopy()) or cycle_mount
+            if restack_old_snapshots:
                 # Unmount old snapshots when mount cycling snapshot mount
                 # and when rotating latest snapshot into old snapshot stack
                 try:
@@ -100,15 +111,22 @@ def remount_union(ctx, rotate_upper=False, cycle_mount=False):
                 system("fsfreeze -f " + union_mntroot)
                 system("fsfreeze -u " + union_mntroot)
 
-            if rotate_upper or cycle_mount:
+            if restack_old_snapshots:
                 # Remount latest snapshot readonly
-                system("mount " + curr_snapshot + " -oremount,ro")
+                if cfg.is_metacopy():
+                    system("mount " + curr_snapshot + " -oremount,ro")
                 mid_layers = ""
                 # Mount old snapshots, possibly pushing the rotated previous snapshot into stack
                 for i in range(ctx.layers_nr() - 1, -1, -1):
-                    mid_layers = upper_mntroot + "/" + str(i) + "/u:" + mid_layers
-                    cmd = ("mount -t overlay overlay " + snapshot_mntroot + "/" + str(i) + "/" +
-                           " -oro,lowerdir=" + mid_layers + curr_snapshot)
+                    snapmnt = snapshot_mntroot + "/" + str(i) + "/"
+                    layer_root = upper_mntroot + "/" + str(i) + "/"
+                    mid_layers = layer_root + "u:" + mid_layers
+                    if cfg.is_metacopy():
+                        cmd = ("mount -t overlay overlay " + snapmnt +
+                               " -oro,lowerdir=" + mid_layers + curr_snapshot)
+                    else:
+                        cmd = ("mount -t overlay overlay " + snapmnt + " " + mntopt +
+                               " -olowerdir=" + lower_mntroot + ",upperdir=" + layer_root + "u,workdir=" + layer_root + "w")
                     system(cmd)
                     if cfg.is_verbose():
                         write_kmsg(cmd);
